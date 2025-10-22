1daccfa6235658b6b1b916e534ae296a51c77263
RifkyTirta16 <rifkyz949@gmail.com>
2025-10-22 15:40:42 +0700

    Input and Scanner

--- Belajar.java
+++ Belajar.java
@@ -1,5 +1,11 @@
+
+import java.util.Scanner;
+
 public class Belajar {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Masukkan nama Anda: ");
+        String nama = scanner.nextLine();
         System.out.println("Halo, Dunia!");
     }
 }
